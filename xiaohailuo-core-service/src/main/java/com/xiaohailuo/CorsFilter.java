7230e3ad259bda07789f750b57cc584d587cee50
duxitao <duxitao@126.com>
2017-12-05 20:32:43 +0800

    添加过滤器，支持跨域

---
+++ xiaohailuo-core-service/src/main/java/com/xiaohailuo/CorsFilter.java
@@ -0,0 +1,37 @@
+package com.xiaohailuo;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * 支持跨域配置
+ * @author duxitao 2017-12-05
+ *
+ */
+@Component
+public class CorsFilter implements Filter {
+
+   
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpServletRequest reqs = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin",reqs.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
+}
